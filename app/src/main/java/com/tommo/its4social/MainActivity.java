490f8eb8d4608fea24cae354abc0153d22ce9e17
tommo.ago <tommo.ago@gmail.com>
2020-03-31 10:53:06 +0200

    aggiunto pulsante fb pt2

--- app/src/main/java/com/tommo/its4social/MainActivity.java
+++ app/src/main/java/com/tommo/its4social/MainActivity.java
@@ -10,18 +10,45 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         printHashKey(this);
+        callbackManager=CallbackManager.Factory.create();
+        loginButton=findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
 
     public static void printHashKey(Context pContext) {
