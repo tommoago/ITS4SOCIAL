20428e58cf3724dabe67930d719ea60039fd4f03
tommo.ago <tommo.ago@gmail.com>
2020-03-31 10:26:55 +0200

    modifica build gradle

--- app/src/main/java/com/tommo/its4social/MainActivity.java
+++ app/src/main/java/com/tommo/its4social/MainActivity.java
@@ -2,8 +2,18 @@ package com.tommo.its4social;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
 import com.facebook.FacebookSdk;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        printHashKey(this);
     }
+
+    public static void printHashKey(Context pContext) {
+        try {
+            PackageInfo info = pContext.getPackageManager().getPackageInfo(pContext.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i("asda", "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("asda", "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e("asda", "printHashKey()", e);
+        }
+    }
+
 }
